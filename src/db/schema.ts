6fe4fe5719499258db08aa4b80c0908cb7b68d24
krutulkamil <krutulkamil@onet.pl>
2023-10-02 14:04:04 +0200

    feat: users table & applications table fixes

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,23 @@
-import { pgTable, timestamp, uuid, varchar } from 'drizzle-orm/pg-core';
+import { pgTable, primaryKey, timestamp, uniqueIndex, uuid, varchar } from "drizzle-orm/pg-core";
 
-export const application = pgTable('application', {
+export const applications = pgTable('applications', {
   id: uuid('id').primaryKey().defaultRandom(),
-  name: varchar('name', { length: 255 }).notNull(),
+  name: varchar('name', { length: 256 }).notNull(),
   createdAt: timestamp('created_at').defaultNow().notNull(),
   updatedAt: timestamp('updated_at').defaultNow().notNull(),
+});
+
+export const users  = pgTable('users', {
+  id: uuid('id').notNull().defaultRandom(),
+  email: varchar('email', { length: 256 }).notNull(),
+  name: varchar('name', { length: 256 }).notNull(),
+  applicationId: uuid('application_id').references(() => applications.id),
+  password: varchar('password', { length: 256 }).notNull(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').defaultNow().notNull(),
+}, (users) => {
+  return {
+    cpk: primaryKey(users.email, users.applicationId),
+    idIndex: uniqueIndex('users_id_index').on(users.id),
+  }
 });
