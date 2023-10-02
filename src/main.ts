c8f08739907a2c86a4309f19b4743064512d4c61
krutulkamil <krutulkamil@onet.pl>
2023-10-02 13:19:01 +0200

    fix: bootstrap shutdown fn

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
 import { buildServer } from './utils/server';
 
+type TAppType = Awaited<ReturnType<typeof buildServer>>;
+
+async function gracefulShutdown({ app }: { app: TAppType }) {
+  await app.close();
+}
+
 async function main() {
   const app = await buildServer();
 
   await app.listen({
     port: 3000,
   });
+
+  const signals = ['SIGINT', 'SIGTERM', 'SIGQUIT'] as const;
+  for (const signal of signals) {
+    process.on(signal, () => {
+      gracefulShutdown({ app });
+    });
+  }
 }
 
 main();
